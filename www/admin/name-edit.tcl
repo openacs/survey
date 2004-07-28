11fbf5321c8a5216b1d28fda46459b0edd0f3b83
Tracy Adams <teadams@alum.mit.edu>
2004-07-28 03:10:23 +0000

    bug 1991 - survey description formatting

--- www/admin/name-edit.tcl
+++ www/admin/name-edit.tcl
@@ -12,27 +12,26 @@ ad_page_contract {
 
     survey_id:integer
     {name ""}
+    {description:html ""}
 }
 
 get_survey_info -survey_id $survey_id
 set survey_name $survey_info(name)
-set survey_description $survey_info(description)
 
 ad_require_permission $survey_id survey_modify_survey
 
 ad_form -name edit-name -form {
     survey_id:key
     {name:text(text) {label "[_ survey.Survey_Name_1]"} {html {size 80}}
-	{value $survey_name}}
-	{description:text(textarea) {label "[_ survey.Description_1]"} 
-	{html {rows 10 cols 65}}
-        {value $survey_description}}
+	{value $survey_info(name)}}
+	{description:text(textarea) {value "HI"}  {label "[_ survey.Description_1]"} 
+	{html {rows 10 cols 65}}}
 } -validate {
     {name {[string length $name] <= 4000}
     "[_ survey.lt_Survey_Name_must_be_l]"
     }
 } -edit_request {
-    set name $survey_name
+    set name $survey_info(name)
 } -edit_data {
     db_dml survey_update ""
     ad_returnredirect "one?[export_vars survey_id]"
